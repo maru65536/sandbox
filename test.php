d3e33a38bbedb20552a6e0d5c1f838c4a11f1014
maru65536 <maru65536@gmail.com>
2020-05-26 20:00:46 +0900

    fix

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo = new PDO('mysql:host=172.17.196.154;dbname=test;charset=utf8','root','3412');
+    $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','3412');
     $qry = $pdo->prepare('select * from test');
     $qry->execute();
     foreach ($qry->fetchAll() as $q) {
