896d22694dbd7e8af3fed71f9512272ef7474885
maru65536 <maru65536@gmail.com>
2020-06-21 18:26:14 +0900

    JOI難易度に対応

--- Chokudaibot.py
+++ Chokudaibot.py
@@ -4,23 +4,26 @@ import time
 import math
 import datetime
 import requests
+import json
+import codecs
 
 # Botのアクセストークン、使用するチャンネルID、ユーザーリスト
-token='hoge and fuga'
+token='hoge and huga'
 channel_id=723157402387611748
 #[DiscordID,AtCoderID]で指定
 users=[
-    [414689564318498816,'maru65536'],
-    [640616185137725453,'irisviel'],
-    [488978370164555776,'potex59049'],
-    [462862915209527298,'yi7242']
+    [414689564318498816,'maru65536','https://img.atcoder.jp/icons/285eb303e7617ede77d1176e1d000ccf.jpg'],
+    [640616185137725453,'irisviel','https://img.atcoder.jp/assets/icon/avatar.png'],
+    [488978370164555776,'potex59049','https://img.atcoder.jp/icons/3b54e96dcdb14dc634fd8bed931c63d3.jpg'],
+    [462862915209527298,'yi7242','https://img.atcoder.jp/icons/ec72028cf0fd8fdf99d2dcfd0d33bc07.png']
     ]
 colors=[0x808080,0x8b4513,0x008000,0x00ffff,0x0000ff,0xffff00,0xffa500,0xff0000]
 
 client = discord.Client()
 
 #AtCoderIDを入れると、ACした問題のリストを返す
-#返り値は[[問題id、タイトル、diff]*いくつか,maxdiff]の形
+#返り値は[[問題id,タイトル,diff,JOIの問題かどうか]*いくつか,maxdiff]の形
+#JOIの問題である場合、diffが存在しない場合はそれぞれJOI難易度、-1を返す
 #現在の時刻からsec秒前までを取得
 def ACProblems(id,sec):
     epochs=int(datetime.datetime.now().timestamp()-sec)
@@ -31,24 +34,29 @@ def ACProblems(id,sec):
     result=requests.get(url).json()
     for dic in result:
         if dic['epoch_second']>=epochs and dic['result']=='AC':
-            tmp.append(dic["problem_id"])
+            tmp.append([dic["problem_id"],dic["contest_id"]])
     url="https://kenkoooo.com/atcoder/resources/merged-problems.json"
     Problemlist=requests.get(url).json()
     url="https://kenkoooo.com/atcoder/resources/problem-models.json"
     difflist=requests.get(url).json()
+    JOI_dic=json.load(codecs.open('Sandbox/JOI.json', 'r', 'utf-8'))
     max_diff=-(10**9)
     for problem_id in tmp:
         for dic in Problemlist:
-            if problem_id==dic["id"]:
+            if problem_id[0]==dic["id"]:
                 title=dic["title"]
                 diff=-1
-                if problem_id in difflist:
-                    diff=int(difflist[problem_id]["difficulty"])
+                isjoi=(problem_id[1][0:2]=='jo')
+                if problem_id[0] in difflist:
+                    diff=int(difflist[problem_id[0]]["difficulty"])
                 #diffがマイナスにならないよう補正
                 if diff<=400 and diff!=-1:
                     diff=int(400/e**((400-diff)/400))
                 max_diff=max(max_diff,diff)
-                AC.append([problem_id,title,diff])
+                JOI_title=list(title.split())[1]
+                if isjoi:
+                    diff=JOI_dic[JOI_title]
+                AC.append([problem_id,title,diff,isjoi])
                 continue
     AC.append(max_diff)
     return AC
@@ -79,14 +87,16 @@ async def loop():
             AC=ACProblems(person[1],86400)
             if len(AC)==1:
                 embed = discord.Embed(title=user.name, description='AtCoderやれって言ったのに...' ,color=0x000000)
+                embed.set_thumbnail(url=person[2])
                 await channel.send(embed=embed)
             else:
                 embed = discord.Embed(title=user.name, description='以下の{}問解きました！えらい！'.format(len(AC)-1) ,color=colors[AC[-1]//400])
                 for Problem in AC:
                     if type(Problem) is int:
+                        embed.set_thumbnail(url=person[2])
                         await channel.send(embed=embed)
                         break
-                    embed.add_field(name=Problem[0],value='{} : diff{}'.format(Problem[1],Problem[2] if Problem[2]!=-1 else 'なし'),inline=0)
+                    embed.add_field(name=Problem[0][0],value='{} : {}{}'.format(Problem[1],'JOI難易度' if Problem[3] else 'diff',Problem[2] if Problem[2]!=-1 else 'なし'),inline=0)
 
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
